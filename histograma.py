3b7c74452730ae704840c03476277cb97ac87c13
DovahChief <pepelimonta@gmail.com>
2016-10-16 22:00:04 -0500

    arreglado lo de etiqueta 'y'

--- histograma.py
+++ histograma.py
@@ -3,10 +3,10 @@ import matplotlib.pyplot as plt
 edades_p = [23,43,55,46,32,123,57,89,90,22,76,64,88]
 bins = [n for n in range(130) if n%10 == 0]
 
-plt.hist(edades_p, bins , histtype = 'bar', rwidth = 0.8)
+plt.hist(edades_p, bins , histtype = 'bar', rwidth = 0.8 , label  ='e')
 
 plt.xlabel('edades')
-plt.ylable('poblacion')
+plt.ylabel('poblacion')
 
 plt.title('histograma de edades')
 plt.legend()
