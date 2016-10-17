708e99d9ff79db49a64baebe725fd916c6f04db8
DovahChief <pepelimonta@gmail.com>
2016-10-16 21:56:44 -0500

    histograma de edades

---
+++ histograma.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+
+edades_p = [23,43,55,46,32,123,57,89,90,22,76,64,88]
+bins = [n for n in range(130) if n%10 == 0]
+
+plt.hist(edades_p, bins , histtype = 'bar', rwidth = 0.8)
+
+plt.xlabel('edades')
+plt.ylable('poblacion')
+
+plt.title('histograma de edades')
+plt.legend()
+plt.show()
