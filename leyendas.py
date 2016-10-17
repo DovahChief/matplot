bad6c77c742756bce59af51fc50414b826f672f0
DovahChief <pepelimonta@gmail.com>
2016-10-16 21:24:12 -0500

    suma a n

--- leyendas.py
+++ leyendas.py
@@ -7,11 +7,13 @@ def fun(x):     #funcion seno
 x = [n for n in range(9)] #seno de solo los enteros
 y = [fun(n) for n in range(9)]
 
-x2 = [n*0.5 for n in range(9)] #seno de solo los enteros
-y2 = [fun(n*0.5) for n in range(9)]
+x2 = [n+0.5 for n in range(9)] #seno de solo los enteros
+y2 = [fun(n+0.5) for n in range(9)]
 
-plt.plot(x,y)
-plt.plot(x2,y2)
+plt.plot(x,y , label ='orig')
+plt.plot(x2,y2 , label = 'seg')
+
+plt.legend()
 
 plt.xlabel('tiempo (t)')
 plt.ylabel('seno(t)')
